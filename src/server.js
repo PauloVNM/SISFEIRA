803450fc34bff9dc084744ecfc1b74697dc69dc4
PauloVNM <paulo.v.n.m.dv@gmail.com>
2026-09-08 00:23:00 -0400

    fix: Auto-commit: 2026-09-08 00:23:00

--- src/server.js
+++ src/server.js
@@ -38,6 +38,6 @@ app.use('/api/relatorios', reportRoutes);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
